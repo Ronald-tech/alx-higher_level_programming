e43883e06d1acb1770526b68b7bc0655b12b56e8
Ronald-tech <ronieqtsang@gmail.com>
2024-02-05 11:51:27 -0800

    ‘9-rectangle.py’

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -18,4 +18,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         '''String representation method.'''
-        return "[Retangle] " + str(self.__width) + "/" + str(self.__height)
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
