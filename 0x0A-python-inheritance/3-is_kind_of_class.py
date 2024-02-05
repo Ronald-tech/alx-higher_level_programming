c4e167fa59fc1d82c27c02e506cd8a23f244859e
Ronald-tech <ronieqtsang@gmail.com>
2024-02-05 10:45:38 -0800

    ‘3-is_kind_of_class.py’

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,6 @@
 '''Module for is_kind_of_class method.'''
 
 
-def inherits_from(obj, a_class):
+def is_kind_of_class(obj, a_class):
     '''Determines if an object is a subclass of a class.'''
     return isinstance(obj, a_class)
