123fe63311004d7cc4d347410e370e11d846053a
Ronald-tech <ronieqtsang@gmail.com>
2024-02-06 09:59:43 -0800

    ‘7-add_item.py’

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
-arglist = list(sys.argv[i:])
+arglist = list(sys.argv[1:])
 
 try:
     old_data = load_from_json_file('add_item.json')
