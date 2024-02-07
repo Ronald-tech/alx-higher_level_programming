0c1dc67fc924d14df66fb85bed1b56252dd6766f
Ronald-tech <ronieqtsang@gmail.com>
2024-02-06 18:55:40 -0800

    ‘11-student.py’

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -24,6 +24,13 @@ class Student:
             return self.__dict__
         my_dict = dict()
         for key, value in self.__dict__.items():
-            if key in attrs :
+            if key in attrs:
                 my_dict[key] = value
         return my_dict
+
+    def reload_from_json(self, json):
+        '''this function replaces all attributes of the student instance with the ones in json argument.
+        '''
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
