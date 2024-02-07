578f69c4addf8abd1b7f723ddc2297c74487ae53
Ronald-tech <ronieqtsang@gmail.com>
2024-02-06 18:57:59 -0800

    ‘10-student.py’

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -24,6 +24,6 @@ class Student:
             return self.__dict__
         my_dict = dict()
         for key, value in self.__dict__.items():
-            if key in attrs :
+            if key in attrs:
                 my_dict[key] = value
         return my_dict
