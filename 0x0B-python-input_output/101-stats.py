0aa7c451557656e55872fbdca49e4e16ad21a203
Ronald-tech <ronieqtsang@gmail.com>
2024-02-24 00:52:45 -0800

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -21,7 +21,7 @@ total_size = i = 0
 
 def printer():
     '''this function prints statistics'''
-    print(f'File size: (total_size)')
+    print(f'File size: {total_size}')
     for key, value in sorted(status_codes.items()):
         if value > 0:
             print('{:s}: {:d}'.format(key, value))
@@ -33,8 +33,8 @@ try:
         if len(splitted_line) >= 2:
             status = splitted_line[-2]
             total_size += int(splitted_line[-1])
-             if status in status_codes:
-                 status_codes[status] += 1
+            if status in status_codes:
+                status_codes[status] += 1
         i += 1
 
         if i % 10 == 0:
